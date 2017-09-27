55f9c843f674124d45c70aff5e03d72955e602a3
Mitchell <mjlewis4@bsu.edu>
2017-09-26 22:46:39 -0400

    Added more test classes, made adjustments. Handled most errors.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
@@ -51,10 +52,10 @@ public class Main extends Application {
                 ObservableList list = FXCollections.observableArrayList(page.getUserList());
 
                 usernameCol.setCellValueFactory {
-                    new PropertyValueFactory<User,String>("username")
+                    new PropertyValueFactory<User,String>("username");
                 };
                 timeCol.setCellValueProperty{
-                    new PropertyValueFactory<Revision,String>("revisions")
+                    new PropertyValueFactory<Revision,String>("revisions");
                 };
 
                 table.setItems(list);
